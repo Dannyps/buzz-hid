90b500397012a1d005c34c3b2c9d9c123bc8d9d5
Daniel Silva <danny@dannyps.net>
2018-01-26 13:22:40 +0000

    Print Buzzers is working

--- buzz.c
+++ buzz.c
@@ -1,8 +1,11 @@
-
+#include <stdbool.h>
 /* Linux */
 #include <linux/types.h>
 #include <linux/input.h>
 #include <linux/hidraw.h>
+
+
+#define BIT(n) (0x01<<(n))
 
 /*
  * Ugly hack to work around failing compilation on systems that don't
@@ -27,10 +30,101 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & BIT(0) ? '1' : '0'), \
+  (byte & BIT(1) ? '1' : '0'), \
+  (byte & BIT(2) ? '1' : '0'), \
+  (byte & BIT(3) ? '1' : '0'), \
+  (byte & BIT(4) ? '1' : '0'), \
+  (byte & BIT(5) ? '1' : '0'), \
+  (byte & BIT(6) ? '1' : '0'), \
+  (byte & BIT(7) ? '1' : '0')
+
+typedef struct{
+	bool light;
+	bool buzzBtn, blueBtn, orangeBtn, greenBtn, yellowBtn;
+} buzz_controller;
+
+typedef struct{
+	buzz_controller controller[4];
+} buzz_hub;
+
 void print_hex(const char *s, unsigned long len){
 	while(len-->0)
 		printf("0x%02x ", (unsigned char) *s++);
 	printf("\n");
+}
+
+void printBuzzButtons(char* c){
+	for(int i = 2; i<5; i++){
+		printf("byte %d "BYTE_TO_BINARY_PATTERN" ", i, BYTE_TO_BINARY(c[i]));
+	}
+	printf("\n");
+}
+
+bool updateStructByRead(buzz_hub* hub, char* c){
+
+	hub->controller[0].blueBtn 		= c[2] & BIT(4);
+	hub->controller[0].orangeBtn 	= c[2] & BIT(3);
+	hub->controller[0].greenBtn 	= c[2] & BIT(2);
+	hub->controller[0].yellowBtn 	= c[2] & BIT(1);
+	hub->controller[0].buzzBtn 		= c[2] & BIT(0);
+
+	hub->controller[1].blueBtn 		= c[3] & BIT(1);
+	hub->controller[1].orangeBtn 	= c[3] & BIT(0);
+	hub->controller[1].greenBtn 	= c[2] & BIT(7);
+	hub->controller[1].yellowBtn 	= c[2] & BIT(6);
+	hub->controller[1].buzzBtn 		= c[2] & BIT(5);
+
+	hub->controller[2].blueBtn 		= c[3] & BIT(6);
+	hub->controller[2].orangeBtn 	= c[3] & BIT(5);
+	hub->controller[2].greenBtn 	= c[3] & BIT(4);
+	hub->controller[2].yellowBtn 	= c[3] & BIT(3);
+	hub->controller[2].buzzBtn 		= c[3] & BIT(2);
+
+	hub->controller[3].blueBtn 		= c[4] & BIT(3);
+	hub->controller[3].orangeBtn 	= c[4] & BIT(2);
+	hub->controller[3].greenBtn 	= c[4] & BIT(1);
+	hub->controller[3].yellowBtn 	= c[4] & BIT(0);
+	hub->controller[3].buzzBtn 		= c[3] & BIT(7);
+
+	if(hub->controller[0].yellowBtn && hub->controller[1].yellowBtn && hub->controller[2].yellowBtn && hub->controller[3].yellowBtn){
+		printf("pressed yellow on all controlers!\n");
+	}
+	return true;
+}
+
+void printHUB(buzz_hub* hub){
+	for(int i=0;i<4;i++){
+		buzz_controller c= hub->controller[i];
+		printf(" 		     ^\n		   / %d \\\n		 / ", i);
+		if(c.buzzBtn)
+			printf("BUZZ");
+		else
+			printf("    ");
+		printf("	 \\\n		/---------\\\n		|  ");
+		if(c.blueBtn)
+			printf("BLUE");
+		else
+			printf("    ");
+		printf("   |\n		|  ");
+		if(c.orangeBtn)
+			printf("ORANGE");
+		else
+			printf("      ");
+		printf(" |\n		|  ");
+		if(c.greenBtn)
+			printf("GREEN");
+		else
+			printf("     ");
+		printf("  |\n		|  ");
+		if(c.yellowBtn)
+			printf("YELLOW");
+		else
+			printf("      ");
+		printf(" |\n		-----------\n\n");
+	}
 }
 
 void getRawName(unsigned long fd, char* buf){
@@ -42,14 +136,27 @@ void getRawName(unsigned long fd, char* buf){
 }
 
 int main(){
-	int res;
+	buzz_hub hub;
+	int fd = open("/dev/hidraw0", O_RDWR);
+	if(fd==-1){
+		fprintf(stderr, "The Buzz controller is not connected!. Exiting.\n");
+		exit(-1);
+	}
 	char buf[256];
-	unsigned int fd = open("/dev/hidraw0", O_RDWR);
+	getRawName(fd, buf);
+	printf("Raw name is %s.\n", buf);
 
 	while(1){
-		char c[10];
-		read(fd, c, 8);
-		print_hex(c, 8);
-		printf("done;\n");
+		char c[5];
+		int ret = read(fd, c, 5);
+		if(ret==-1){
+			fprintf(stderr, "The Buzz controller was disconnected. Exiting.\n");
+			exit(-1);
+		}
+		//printf("got %d from read\n", ret);
+		//printBuzzButtons(c);
+		updateStructByRead(&hub, c);
+		printHUB(&hub);
+		//printf("done;\n");
 	}
 }
